c42939545fde6ca621186ab0c4eb9edf801ca89f
Neal L <rlawton.cs@gmail.com>
2024-01-13 23:24:00 -0800

    Fixed intents line, which was causing discord bot to not respond to commands

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import discord
 import os
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents = discord.Intents.all()
+client = discord.Client(intents = intents)
 
-@client.event 
+@client.event
 async def on_ready():
-  print("We have logged in as {0.user}".format(client))
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith("$hello"):
+  if message.content.startswith("!hello"):
     await message.channel.send("Hello!")
 
 client.run(os.environ['TOKEN'])
